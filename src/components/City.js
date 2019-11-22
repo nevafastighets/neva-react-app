3c86a8342532a1ea2f530a513863df7c065fe29b
Alexander Wassbjer <alexanderwassbjer@outlook.com>
2019-11-22 08:49:15 +0100

    Add hover animations for cities and properties

--- src/components/City.js
+++ src/components/City.js
@@ -2,6 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import posed from "react-pose";
+
+const Box = posed.div({
+  hoverable: true,
+  pressable: true,
+  init: {
+    scale: 1
+  },
+  hover: {
+    scale: 1.05
+  },
+  press: {
+    scale: 1
+  }
+});
 
 const StyledHero = styled.div`
   height: 100%;
@@ -18,12 +33,14 @@ export default function City({ city }) {
 
   return (
     <Link className="city-margin" to={`/hitta-bostad/${cityslug}`}>
-      <div className="city bd-highlight">
-        <StyledHero img={featuredImage}>
-          <div className="overlay-image" />
-          <span className="city-grid-title">{title}</span>
-        </StyledHero>
-      </div>
+      <Box className="box">
+        <div className="city bd-highlight">
+          <StyledHero img={featuredImage}>
+            <div className="overlay-image" />
+            <span className="city-grid-title">{title}</span>
+          </StyledHero>
+        </div>
+      </Box>
     </Link>
   );
 }
--- src/components/Property.js
+++ src/components/Property.js
@@ -2,6 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import posed from "react-pose";
+
+const Box = posed.div({
+  hoverable: true,
+  pressable: true,
+  init: {
+    scale: 1
+  },
+  hover: {
+    scale: 1.05
+  },
+  press: {
+    scale: 1
+  }
+});
 
 const StyledHero = styled.div`
   height: 15rem;
@@ -19,13 +34,15 @@ export default function Property({ property }) {
       className="property-margin"
       to={`/hitta-bostad/${city.fields.cityslug}/${slug}`}
     >
-      <div className="property bd-highlight">
-        <StyledHero img={featuredImage} />
-        <div className="property-grid-content">
-          <span className="property-grid-title">{title}</span>
-          <span className="property-grid-desc">{description}</span>
+      <Box className="box">
+        <div className="property bd-highlight">
+          <StyledHero img={featuredImage} />
+          <div className="property-grid-content">
+            <span className="property-grid-title">{title}</span>
+            <span className="property-grid-desc">{description}</span>
+          </div>
         </div>
-      </div>
+      </Box>
     </Link>
   );
 }
